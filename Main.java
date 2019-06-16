913c591ad0c4a4217409e52e974f9fb443eaa859
AnthonyBFreay <anthonyfreay@gmail.com>
2019-06-15 22:37:11 -0400

    Add skeleton methods

--- Main.java
+++ Main.java
@@ -14,11 +14,15 @@ public class Main {
 
         goToProfilePage();
 
+        //listFollowers();
+
+        //listFollowing();
+
         //logOutOfInstagram();
 
     }
 
-    public static void openChromeBrowser(){
+    public static void openChromeBrowser() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","/Users/anthonyfreay/Dev/java/selenium-java/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
@@ -26,9 +30,12 @@ public class Main {
 
         // open new chrome browser
         driver = new ChromeDriver(options);
+
+        // simulate human delay
+        Thread.sleep(2000);
     }
 
-    public static void logInToInstagram(){
+    public static void logInToInstagram() throws InterruptedException {
         // go to instagram login screen
         driver.get("https://www.instagram.com/accounts/login/");
 
@@ -46,18 +53,29 @@ public class Main {
 
         // click 'log in' button
         driver.findElement(By.xpath("//*[@id=\'react-root\']/section/main/div/article/div/div[1]/div/form/div[4]/button")).click();
+
+        // wait for 'notification modal' to pop up
+        Thread.sleep(2000);
     }
 
     public static void goToProfilePage() throws InterruptedException {
-        // wait for 'notification modal' to pop up
-        Thread.sleep(1500);
-
         // click 'not now'
         driver.findElement(By.xpath("/html/body/div[3]/div/div/div[3]/button[2]")).click();
 
         // click profile button
         driver.findElement(By.xpath("//*[@id=\'react-root\']/section/nav/div[2]/div/div/div[3]/div/div[3]/a/span")).click();
+
+        // simulate human delay
+        Thread.sleep(2000);
     }
+
+    public static void listFollowers() throws InterruptedException {
+        driver.get("https://www.instagram.com/anthonyfreay/following/");
+        System.out.println(driver.getCurrentUrl()); // returns https://www.instagram.com/anthonyfreay/
+        Thread.sleep(2000);
+    }
+
+    public static void listFollowing(){}
 
     public static void logOutOfInstagram(){
         // click gear button
